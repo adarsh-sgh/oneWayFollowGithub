de40a67142c556d6baa0439149d3eabdc586a039
Sahil Verma <79751075+sahil19-19@users.noreply.github.com>
2022-10-25 16:58:49 +0000

    Add CDN links for Bootstrap, Font Awesome, add links for Google fonts (#1)
    * add CDN links for Bootstrap, Font Awesome, add links for Google fonts
    
    * Create README.md
    
    * Update README.md
    
    * added new line at the end in index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ document.querySelector('#submit').addEventListener('click', onUsernameSubmit)
 async function onUsernameSubmit(params) {
   const username = document.querySelector('#username').value
   const {onlyFollowers,onlyFollowing} = await oneWayFollowers(username)
-document.querySelector('#result').innerHTML = "<h3>only follower</h3> <br>"+onlyFollowers.join('<br>')+ '<h3>only following</h3>' + onlyFollowing.join('<br>')
+document.querySelector('#result').innerHTML = "<h3>User's only follower</h3> <br>"+onlyFollowers.join('<br>')+ "<h3>User's only following</h3>" + onlyFollowing.join('<br>')
   console.log(onlyFollowers,onlyFollowing)
 }
